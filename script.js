e96fe711e61d833f7c2ade1458636b1207c4cb23
HaYeon-Jang <73008049+HaYeon-Jang@users.noreply.github.com>
2020-12-15 12:16:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-function counter(){
-    var dday = new Date("Dec 12,2019,09:00:00").getTime(); //디데이
-	setInterval(function(){
-		var now = new Date(); //현재 날짜 가져오기
-		var distance = dday - now;
-		var d = Math.floor(distance / (1000 * 60 * 60 * 24));
-		var h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		var m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var s = Math.floor((distance % (1000 * 60)) / 1000);
-		if(s < 10){
-			s = '0'+s;
-		}
-		$('.content').html(d+':'+h+':'+m+':'+s)
-	}, 1000);
-}
-counter();
+var now = new Date();
+var then = new Date("jan 01,2021");  
+var gap = then.getTime() - now.getTime();
+gap = Math.floor(gap / (1000 * 60 * 60 * 24));
+document.write("2020 년까지 " + gap + " 일 남았습니다!");
