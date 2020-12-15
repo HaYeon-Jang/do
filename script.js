19337dcf8d490952dda04040348b7cce4e935df2
HaYeon-Jang <73008049+HaYeon-Jang@users.noreply.github.com>
2020-12-15 11:07:00 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function counter(){
+    var dday = new Date("Dec 12,2019,09:00:00").getTime(); //디데이
+	setInterval(function(){
+		var now = new Date(); //현재 날짜 가져오기
+		var distance = dday - now;
+		var d = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var s = Math.floor((distance % (1000 * 60)) / 1000);
+		if(s < 10){
+			s = '0'+s;
+		}
+		$('.content').html(d+':'+h+':'+m+':'+s)
+	}, 1000);
+  
+  counter();
